7b65034904c4963df1c84d0e4d6d034ad967c3f0
yensen-arch <officialdevain25@gmail.com>
2025-03-11 18:34:02 +0530

    tsx

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,13 +10,14 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-  {
+  ...compat.config({
+    extends: ['next'],
     rules: {
-      "@typescript-eslint/no-unused-vars": ["warn", { "argsIgnorePattern": "^_", "varsIgnorePattern": "^_" }],
+      'react/no-unescaped-entities': 'off',
+      '@next/next/no-page-custom-font': 'off',
     },
-  },
-];
+  }),
+]
 
 export default eslintConfig;
 
