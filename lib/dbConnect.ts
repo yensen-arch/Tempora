c215c60b4c09f1163f88872d891052ee8ddbfd47
yensen-arch <officialdevain25@gmail.com>
2025-03-26 11:05:02 +0530

    G

--- lib/dbConnect.ts
+++ lib/dbConnect.ts
@@ -3,14 +3,13 @@ import dotenv from "dotenv";
 
 const dbConnect = async () => {
   dotenv.config();
-  console.log("heres the env var", process.env.MONGODB_URI);
   if (mongoose.connection.readyState >= 1) return;
-
+  const uri = process.env.MONGODB_URI;
+  if (!uri) {
+    throw new Error("MONGODB_URI is not defined");
+  }
   try {
-    await mongoose.connect(process.env.MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(uri); 
     console.log("MongoDB connected");
   } catch (error) {
     console.error("Error connecting to MongoDB:", error);
